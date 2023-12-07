b6718bcc6618dd5661f46459e8f9058b592beeae
soapity <mrows9356@gmail.com>
2023-12-07 17:34:50 -0500

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import ClassBox from "./ClassBox.svelte";
+
+
+</script>
+
+<section class = "w-100 p-5 d-flex justify-content-center">
+	<ClassBox />
+	<ClassBox />
+	<ClassBox />
+</section>
+<section class = "w-100 p-2 d-flex">
+
+	
+</section>
+<style>
+
+</style>
---
+++ src/routes/ClassBox.svelte
@@ -0,0 +1,49 @@
+<div class = "classBox p-4 d-flex flex-column">
+    <span class = "w-100 d-flex px-3">
+        AP Physics C
+        &nbsp;
+        <button class = "btn btn-outline-success ms-auto"><i class="fa-solid fa-plus"></i></button>
+    </span>
+    <hr>
+
+    <span class = "d-flex text-break">
+        <input type = "checkbox"/>
+        AP Classroom
+    </span>
+    <span class = "d-flex text-break">
+        <input type = "checkbox"/>
+        sadsdddddddddddddddddddddddddddddddddddddd
+    </span>
+    <span class = "d-flex text-break">
+        <input type = "checkbox"/>
+        sadsdddddddddddddddddddddddddddddddddddddd
+    </span>
+    <span class = "d-flex text-break">
+        <input type = "checkbox"/>
+        sadsdddddddddddddddddddddddddddddddddddddd
+    </span>
+</div>
+
+<style>
+	input[type="checkbox"] {
+		scale: 1.5;
+		margin-right: 20px;
+	}
+	.classBox {
+		border: 2px dotted green;
+		width: 340px;
+		height: 300px;
+		overflow-y: auto;
+        margin: 0px 20px 0px 20px;
+		/* box-shadow: 10px 10px #d3d3d3; */
+	}
+    .classBox::-webkit-scrollbar {
+    display: none;
+    }
+
+    /* Hide scrollbar for IE, Edge and Firefox */
+    .classBox {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+    }
+</style>
